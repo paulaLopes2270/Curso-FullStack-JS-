c089004f5ebdbd389844cc59f11608b435a448d3
paulaLopes2270 <plnhschltz@gmail.com>
2021-08-24 10:10:31 -0300

    exercício 3

--- js essencial/Ex3/script.js
+++ js essencial/Ex3/script.js
@@ -4,18 +4,22 @@ let velocidade = 0
 
 let velocidadeEscolhida = prompt("A que velocidade você gostaria de acelerar a nave?")
 
-confirm(`confirma a velocidade ${velocidadeEscolhida}?`)
+let confirmVelocidade = confirm(`confirma a velocidade ${velocidadeEscolhida}?`)
 
-if (velocidadeEscolhida < 0) {
+if (confirmVelocidade) {
+    velocidade = velocidadeEscolhida
+}
+
+if (velocidade <= 0) {
     alert("Nave Está parada.Considere partir e aumentar a velocidade")
-} else if (velocidadeEscolhida < 40) {
+} else if (velocidade < 40) {
     alert("Você está devagar, podemos aumentar mais")
-} else if ((velocidadeEscolhida >= 40) && (velocidadeEscolhida <= 80)) {
+} else if ((velocidade >= 40) && (velocidade <= 80)) {
     alert("Parece uma boa velocidade para manter")
-} else if ((velocidadeEscolhida >= 80) && (velocidadeEscolhida < 100)) {
+} else if ((velocidade >= 80) && (velocidade < 100)) {
     alert("Velocidade Alta. Considere diminuir")
 } else {
     alert("Velocidade perigosa. Controle automático forçado")
 }
 
-alert(`Piloto: ${nome} \nvelocidade atual: ${velocidadeEscolhida}`)
+alert(`Piloto: ${nome} \nvelocidade atual: ${velocidade}`)
