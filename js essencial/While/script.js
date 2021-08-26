bb0d8adc42b23c99320c1b9d21e60a65f0daf9f1
paulaLopes2270 <plnhschltz@gmail.com>
2021-08-26 20:27:30 -0300

    While

--- js essencial/While/script.js
+++ js essencial/While/script.js
@@ -5,3 +5,15 @@ while (velocity <= 100) {
     console.log(`Acelerando: Estamos a ${velocity} km/s`)
     velocity += aceleration // velocity = velocity + acceleration
 }
+
+let constellation = "Andromeda"
+let pos = 0
+let constellationLength = constellation.length
+
+while (pos < constellationLength) {
+    if (constellation[pos] == "a" || constellation[pos] == "A") {
+        console.log(pos)
+
+    }
+    pos += 1
+}
