e2440684214c8dcb52c68cea6cee105a31624171
paulaLopes2270 <plnhschltz@gmail.com>
2021-08-26 18:17:53 -0300

    While

---
+++ js essencial/While/script.js
@@ -0,0 +1,7 @@
+let velocity = 50
+let aceleration = 5
+
+while (velocity <= 100) {
+    console.log(`Acelerando: Estamos a ${velocity} km/s`)
+    velocity += aceleration // velocity = velocity + acceleration
+}
