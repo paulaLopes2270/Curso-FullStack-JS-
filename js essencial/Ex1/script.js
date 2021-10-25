a7c90cd5306143528ee8c0a2d77432175f3062f7
paulaLopes2270 <plnhschltz@gmail.com>
2021-10-25 18:20:58 -0300

    .

--- js essencial/Ex1/script.js
+++ js essencial/Ex1/script.js
@@ -3,4 +3,4 @@ alert("Bem vindo! A seguir pediremos que informe alguns dados")
 let name = prompt("Qual é o seu nome?")
 let idade = prompt("Informe a sua idade?")
 
-let ageConfirm = confirm("Sua idade é " + idade + " anos")
+let ageConfirm = confirm(`Sua idade é  ${idade} anos`)
